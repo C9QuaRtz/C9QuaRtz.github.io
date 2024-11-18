341f88f127690e10fc4d80bcd5586ee9329e1f35
C9QuaRtz <com99999@outlook.com>
2024-11-19 01:39:26 +0800

    Site updated: 2024-11-19 01:39:25

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1731951427744";
+const VERSION = "1731951565304";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
